89966935dc5d88e09f106ca4557fa72e0f4df50d
Djalma <djalma.msmansueto@senacsp.edu.br>
2025-01-30 21:14:34 -0300

    Alteração raio

--- atividade1.js
+++ atividade1.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-const raio1 = parseFloat(prompt('Qual o raio do circulo?:'))
+const raio1 = parseFloat(prompt('Qual o raio do circulo do raio?:'))
 var area =3.14 * (raio1*raio1)
 var pi = 3.14
 var resultado =(3.14 * (raio1*raio1))
